dc60bd5e56a742f51b722bc6cb2c1ca28e649dbf
WebDevAman <amanpandeyfiverr@gmail.com>
2022-10-27 20:20:14 +0530

    /blog, /blog/[category], /blog/[category]/[article]

--- components/blog/BlogCard.jsx
+++ components/blog/BlogCard.jsx
@@ -2,22 +2,27 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { BiTimeFive } from 'react-icons/bi'
+import { BsArrowRight } from 'react-icons/bs'
 import { FiUser } from 'react-icons/fi'
 
 const BlogCard = ({ data }) => {
-    const { title, description, slug, category, authorImage, imageUrl, authorName, date } = data
     const truncate = (text, slicingInd) => {
         const sliced = text.length > slicingInd ? text.slice(0, slicingInd) + '...' : text
         return sliced
     }
     return (
-        <Link href={slug}>
+        <Link href={`/blog/[category]/[slug]`} as={`/blog/${data.category.slug}/${data.slug}`}>
             <a className='flex group flex-col h-fit max-w-[25rem]  overflow-hidden'>
                 <div className="relative aspect-video min-h-[250px] w-full">
-                    <Image src={imageUrl} layout='fill' objectFit='cover' className=' group-hover:scale-105 transition-all duration-300' alt={'image'} />
+                    <Image src={data.content[0].src} layout='fill' objectFit='cover' className=' group-hover:scale-105 transition-all duration-300' alt={'image'} />
+                    <Link href={`/blog/[category]`} as={`/blog/${data.category.slug}`}>
+                        <a className="absolute rounded-full text-xs font-semibold py-1.5 px-3 right-2 bottom-2 bg-white opacity-90 hover:opacity-100">
+                            {data.category.title}
+                        </a>
+                    </Link>
                 </div>
                 <div className="py-4 flex flex-col space-y-2">
-                    <div className="flex items-center space-x-4">
+                    {/* <div className="flex items-center space-x-4">
                         <div className="flex items-center space-x-2">
                             <BiTimeFive className='text-themeColor' />
                             <span className='text-base opacity-90'>{date}</span>
@@ -27,15 +32,16 @@ const BlogCard = ({ data }) => {
                             <span className='text-base opacity-90'>{authorName}</span>
                         </div>
 
-                    </div>
+                    </div> */}
                     <h2 className="text-2xl link-blog group-hover:text-themeColor font-semibold">
-                        {truncate(title, 60)}
+                        {truncate(data.content.filter(x => x.type === 'h1')[0].text || '', 60)}
                     </h2>
                     <p className="text-base font-medium opacity-90">
-                        {truncate(description, 150)}
+                        {truncate(data.content.filter(x => x.isDesc === true)[0].text || '', 150)}
                     </p>
-                    <div className="border-t flex space-x-3 items-center border-gray-200 py-2">
-                        <div className="py-1 px-2 bg-blue-300 rounded-sm uppercase text-sm font-semibold text-blue-800">{category}</div>
+                    <div className="border-t flex gap-2 hover:text-themeColor hover:gap-4 transition-all duration-300 items-center pt-2">
+                        <span>Read Article</span>
+                        <BsArrowRight />
                     </div>
                 </div>
             </a>
