5f81972304295af204634b44a9d0c1603417c37e
vikasswaminh <67453408+vikasswaminh@users.noreply.github.com>
2023-01-06 12:06:37 +0530

    Update TopBar.jsx

--- components/Navbar/TopBar.jsx
+++ components/Navbar/TopBar.jsx
@@ -5,7 +5,7 @@ const TopBar = () => {
     return (
         <div className='py-2 text-white w-full  bg-themeColor'>
             <div className="flex text-xs md:text-base items-center font-semibold space-x-2 w-fit mx-auto">
-                <p>40% Discount on all courses till 28th December 2022- New Year Offer</p>
+                <p>40% Discount on all courses till 25th Jan 2023- New Year Offer</p>
                 <Link href='/networkers-home-contact-us' className='hover:underline'>Call Us</Link>
             </div>
         </div>
